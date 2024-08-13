d9690af2c67d3cffae1975e7526b7f3fe59fcd21
mmarambana023 <mmarambana023@student.wethinkcode.co.za>
2024-08-13 15:46:29 +0200

    Completing caasaer cipher

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A list of different short to long projects and tools impemented using python
+**List Of Projects**
+- Caesar Cipher
+    A simpler cipher that takes a text and encrypts and decrptys given a cipher key
--- projects/security/encryption/caeser_shift/main.py
+++ projects/security/encryption/caeser_shift/main.py
@@ -1,1 +1,39 @@
+import string
 
+class cipher():
+    session_id=""
+    #has default of 1
+    cipher_rotation =1
+    keys = string.printable
+
+    def __init__(self,rotation):
+        self.cipher_rotation =rotation
+    
+   
+    def encrpyt(self,text):
+        cipher_text= ""
+        for i in text:
+            index = self.keys.find(i)
+            new_index = (index+self.cipher_rotation) % 100
+            cipher_text += self.keys[new_index]
+        return cipher_text
+    
+    
+    def decrpyt(self,text):
+        plain_text =""
+        for i in text:
+            index = self.keys.find(i)
+            new_index =(index-self.cipher_rotation)%100
+            plain_text+=self.keys[new_index]
+        return plain_text
+
+
+    def terminate(self):
+        self.session_id=-1
+
+
+if __name__=="__main__":
+    myCipher = cipher(5)
+    text = "Very simple encryption"
+    print(myCipher.encrpyt(myCipher.encrpyt(text)))
+    
