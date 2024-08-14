23f1be3f85b1b315506ea75ca08fce7b17ee848d
Mbuja <mmarambana023@student.wethinkcode.co.za>
2024-08-14 22:43:39 +0200

    Date and number extraction in webscraper

--- projects/webscraping/webscraper.py
+++ projects/webscraping/webscraper.py
@@ -6,8 +6,36 @@ def scrape()->requests:
     r = requests.get("https://49s.events/lunchtime")
     return r
 
+
+def extract_dates(soup:bs):
+    data = soup.find_all('tr')
+    headers =[]
+    for var in data:
+        if(var.td):
+            headers.append(str(var.td.get_text().strip()))
+    #print(headers)
+    return headers
+
+
+def extract_numbers(soup:bs):
+    data = soup.find_all('div',class_ = 'result')
+    numbers = []
+    num_lists =[]
+    for num in data:
+        if num:
+            numbers.append(int(num.get_text()))
+    while(len(numbers)!=0):
+        temp_list = numbers[0:7]
+        num_lists.append(temp_list)
+        numbers = numbers[7::]
+    #print(num_lists)
+    return num_lists
+
 if __name__ == "__main__":
     response = scrape()
     soup = bs(response.content,'html.parser')
     content = soup.find_all('tr')
-    print(content)
+    extract_dates(soup)
+    extract_numbers(soup)
+    print(dict(zip(extract_dates(soup),extract_numbers(soup))))
+    #print(content)
